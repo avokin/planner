29e262301ba6316fd0eb467277e883e7b3d6dcf9
Andrey Vokin <andrey.vokin@gmail.com>
2017-05-18 09:32:20 +0200

    Tasks tests fixed

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -2,35 +2,36 @@ require 'test_helper'
 
 class TasksControllerTest < ActionDispatch::IntegrationTest
   setup do
+    @day = days(:one)
     @task = tasks(:one)
   end
 
   test "should get index" do
-    get tasks_url, as: :json
+    get day_tasks_url(@day), as: :json
     assert_response :success
   end
 
   test "should create task" do
     assert_difference('Task.count') do
-      post tasks_url, params: { task: { day_id: @task.day_id, finished: @task.finished, name: @task.name } }, as: :json
+      post day_tasks_url(@day), params: { task: { day_id: @task.day_id, finished: @task.finished, name: @task.name } }, as: :json
     end
 
     assert_response 201
   end
 
   test "should show task" do
-    get task_url(@task), as: :json
+    get day_task_url(@day, @task), as: :json
     assert_response :success
   end
 
   test "should update task" do
-    patch task_url(@task), params: { task: { day_id: @task.day_id, finished: @task.finished, name: @task.name } }, as: :json
+    patch day_task_url(@day, @task), params: { task: { day_id: @task.day_id, finished: @task.finished, name: @task.name } }, as: :json
     assert_response 200
   end
 
   test "should destroy task" do
     assert_difference('Task.count', -1) do
-      delete task_url(@task), as: :json
+      delete day_task_url(@day, @task), as: :json
     end
 
     assert_response 204
