d1af31833f97bdbee4113864d8a89e1f55f29d45
Andrey Vokin <andrey.vokin@gmail.com>
2017-09-23 12:42:07 +0200

    Fixed NPE

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -11,7 +11,8 @@ class DaysController < ApplicationController
   # GET /days/1
   def show
     sprint = Sprint.where(["`from` <= :day_id and :day_id <= `to`", {:day_id => @day.id}]).first
-    sprint_field = {sprint_id: sprint.id}
+    sprint_id = sprint.nil? ? nil : sprint.id
+    sprint_field = {sprint_id: sprint_id}
 
     day_data = JSON::parse(@day.to_json(:include => [:tasks])).merge(sprint_field)
     render json: day_data
